5f4dd32065b1980b60f2c9ee9bcf50e766cd626f
Tulio Coelho <tulioopc@gmail.com>
2017-06-03 11:32:11 -0300

    Refactoring reducers and adding comments

--- client/reducers/reducerCurrentUser.js
+++ client/reducers/reducerCurrentUser.js
@@ -1,9 +1,22 @@
+/**
+* This reducer is responsible to return a JSON of the current user.
+*
+* @summary Reducer for update state with current user.
+*/
+
+
 import { CONNECT_USER } from '../actions/index';
 
 export default function (state = { }, action) {
   switch (action.type) {
-    case CONNECT_USER:
-      return action.payload;
+    case CONNECT_USER: {
+      const USER = action.payload;
+      if (USER) {
+        return USER;
+      } else {
+        return state;
+      }
+    }
     default:
       return state;
   }
