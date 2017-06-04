9efab44b3183625830ff6009a75f831bf7f3c288
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2017-06-04 20:53:57 -0300

    New Debug Methods for development use; Minor enhancements in userMethods.

--- server/userMethods.js
+++ server/userMethods.js
@@ -10,11 +10,17 @@
 Meteor.methods({
   // This function is responsible for finding out if the user is the professor/admin
   'user.isAdmin': () => {
-    const actualUser = Meteor.user();
+    const user = Meteor.user();
     let isAdmin = false;
 
-    if (actualUser !== null) {
-      isAdmin = actualUser.profile.is_teacher;
+    /* If the user is not null and if the user is an admin,
+     * the isAdmin variable will be set to true.
+     */
+    const userAdminStatus = user.profile.is_teacher;
+    const userValid = (user !== null && userAdminStatus !== null);
+
+    if (userValid) {
+      isAdmin = userAdminStatus;
     } else {
       // Do nothing - isAdmin already false.
     }
